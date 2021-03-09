2e37a2209734debcdeab20c2ce2b852aa2dd2c54
Akshat <masterakshata@gmail.com>
2021-03-09 19:13:49 +0530

    implemnet delte button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 let myLibrary = [];
 const booksContainer = document.getElementById('books-container')
 const addBookForm = document.getElementById('addBookForm');
+const books = document.querySelectorAll('[data-id]')
 function showAddBookForm() {
   addBookForm.classList.toggle('d-none')
 }
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id = myLibrary.length + 1) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
+  this.id = id
   this.info = function () {
     return (title + " by " + author + ", " + pages + " pages, " + read)
   }
@@ -16,6 +18,9 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read)
   myLibrary.push(book)
+}
+function deleteBook(event){
+  console.log(event.target)
 }
 
 addBookToLibrary('The Dark Knight', 'Bruce Wayne', 20, true)
@@ -27,15 +32,23 @@ myLibrary.forEach((book) => {
 })
 
 function displayBook(book) {
+
   const bookCard = `
-  <div class="card mr-4 mt-2" style="width: 18rem;">
+  <div class="card mr-4 mt-2" style="width: 18rem;" data-id="card-${book.id}">
     <div class="card-body">
       <h5 class="card-title">${book.title}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
       <p class="card-text">${book.pages}</p>
-      <a href="#" class="card-link">Card link</a>
-      <a href="#" class="card-link">Another link</a>
+      
+      <button class="btn btn-danger">Remove</button>
     </div>
-</div>`
-booksContainer.innerHTML += bookCard
+  </div>
+`
+  booksContainer.innerHTML += bookCard
+  const el = document.querySelectorAll(`[data-id] button.btn-danger`).forEach((el)=>{
+    el.addEventListener('click',(e)=>{
+      deleteBook(e)
+    })
+  })
+
 }
