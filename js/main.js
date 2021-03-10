73b9e9ea8ed501c45bb485b4a477cf655c89bd76
Akshat <masterakshata@gmail.com>
2021-03-10 14:54:59 +0530

    complete toggleRead function

--- js/main.js
+++ js/main.js
@@ -50,14 +50,29 @@ function addBookToLibrary(title, author, pages, read) {
 }
 function deleteBook(event){
   const book = event.target.parentElement.parentElement
-  const index = book.dataset.id.split('-')[1] - 1
+  const id = book.dataset.id.split('-')[1]
+  let index;  
+  myLibrary.forEach((book,idx)=>{
+    if(book.id == id){
+      index = idx
+    }
+  })
   myLibrary.splice(index, 1);
   book.remove()
 }
 
-function readStatus(event) {
+function toggleStatus(event) {
   const book = event.target.parentElement.parentElement
-  const index = book.dataset.read.
+  const id = book.dataset.id.split('-')[1]
+  let index ;
+   myLibrary.forEach((book,idx)=>{
+    if(book.id == id) {
+      index = idx
+    }
+  })
+  myLibrary[index].read = !myLibrary[index].read
+  const readEl = document.querySelector(`[data-id = card-${id}] p.read-status`)
+  readEl.innerText = readEl.innerText == 'Read' ? 'Unread' : 'Read'
 
 }
 
@@ -70,7 +85,7 @@ function displayBook(book) {
       <h5 class="card-title">${book.title}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
       <p class="card-text">${book.pages}</p>
-      <p class="card-text">${book.read ? "Read" : "Unread"}</p>
+      <p class="card-text read-status">${book.read ? "Read" : "Unread"}</p>
       
       <button class="btn btn-danger">Remove</button>
       <button class="btn btn-success" id="read">Read</button>
@@ -85,7 +100,7 @@ function displayBook(book) {
   })
   const rd = document.querySelector('#read')
   rd.addEventListener('click', (e) => {
-    console.log(e.target)
+    toggleStatus(e)
   })
   
 
